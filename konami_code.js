bc8c3f0673cb5b1ba9430d5ba331d0faa4dbb4ec
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 20:25:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function initKeyboardEvent(e) {
-  const key = parseInt(e.detail || e.which);
+function init(e) {
+  const triggerKeyDown = parseInt(e.detail || e.which);
  
 }
