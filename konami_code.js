57fc46a5056eddc5a5b43310678d3bc309467803
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 20:47:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0; 
-  document.body.addEventListener('keydown', function(e){
+  document.body.addEventListener('keydown', onKeyDownHandler, function(e){
   const key = parseInt(e.which || e.detail);
  if (code[index] === key) { index++;
  if (index === code.length) { alert('Hurray!');
