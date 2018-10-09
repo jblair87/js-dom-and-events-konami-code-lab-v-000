dbcd00543a912835f857571a29c37db92cdf8269
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 20:17:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-window.alert = expect.createSpy()
-for (let i = 0, l = code.length; i < l; i++) {
-  triggerKeyDown(code[i])
+function triggerKeyDown(e) {
+const {
+ 
 }
 }
