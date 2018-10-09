c10bf54de58bbad4885d52c1a08b957201fa85d6
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 20:35:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,6 +4,6 @@ function init() {
   let index = 0; 
   document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.which || e.detail);
- if (code[index] === key)   index++;
- if (index === code.length) alert('Hurray!')
+ if (code[index] === key) index++;
+ if (index === code.length) alert('Hurray!');
 }
